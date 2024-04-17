f5d931c7f46253888a2ee93d9e597a2d2480f67d
danilo-polesi <danilopolesk8@gmail.com>
2024-04-17 15:13:09 -0300

    first commit

---
+++ Conversão de moeda/Conversão de moeda/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Conversão_de_moeda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Double QuantiaDolar, TaxaCambio, QuantiaMoeda;
+
+            Console.Write("Digite a quantia em dólares: ");
+            QuantiaDolar = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Digite o valor de um dólar em moeda estrangeira (por exemplo, euros): ");
+            TaxaCambio = Convert.ToDouble(Console.ReadLine());
+            QuantiaMoeda = QuantiaDolar * TaxaCambio;
+            Console.Write(QuantiaDolar + " dólares equivalem a " + QuantiaMoeda + " moeda estrangeira.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Conversãodemoeda.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+
+		Double QuantiaDolar, TaxaCambio, QuantiaMoeda;
+
+		Console.Write("Digite a quantia em dólares: ");
+		QuantiaDolar = Convert.ToDouble(Console.ReadLine());
+		Console.Write("Digite o valor de um dólar em moeda estrangeira (por exemplo, euros): ");
+		TaxaCambio = Convert.ToDouble(Console.ReadLine());
+		QuantiaMoeda = QuantiaDolar * TaxaCambio;
+		Console.Write(QuantiaDolar+ " dólares equivalem a " +QuantiaMoeda+ " moeda estrangeira.");
+		Console.ReadKey();
+	}
+}
